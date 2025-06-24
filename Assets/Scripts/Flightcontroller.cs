f492a650f8fb5fbcc9ad4cc675384f7ff945e648
jorisvanperlo <159013746+jorisvanperlo@users.noreply.github.com>
2025-06-25 01:15:01 +0200

    removed a bunch of arbetrary stats and swaped them for real units. general fixes and improvements

--- Assets/Scripts/Flightcontroller.cs
+++ Assets/Scripts/Flightcontroller.cs
@@ -1,109 +1,161 @@
-using UnityEngine;
+﻿using UnityEngine;
 using TMPro;
+using UnityEngine.Windows;
 
 public class Flightcontroller : MonoBehaviour
 {
     // General info and input
-
+    private Rigidbody rb;
+    public float mass_Kg;
     public float throttleIncrement = 0.1f;
-    public float maxThrust = 200f;
-    public float responsiveness = 10f;
+    public float enginePower_Hp = 200f;
+    public float rollResponsiveness, pitchResponsiveness, yawResponsiveness;
     public float liftMultiplier = 1.0f;
+    private float roll, pitch, yaw;
 
+    // Controll Surfaces
+    public GameObject[] elevators, rudder;
+    public GameObject aileronL, aileronR;
+    public float controlSurfRotSpeed;
+    public float aileronMaxRot, elevatorMaxRot, rudderMaxRot;
+    private float currentAileronAngle, currentElevatorAngle, currentRudderAngle;
+
+    // Flaps
+    public GameObject[] flaps;
+    public float flapDeploySpeed;
+    public Vector3 flapDeployAngle, flapFoldedAngle;
+
+    // Engine Force
     private float throttle;
     private float currentThrust = 0f;
     public float accelerationRate = 1f;
-    public float decelerationRate = 1f;
+    public float decelerationRate = 1f; 
 
-    private float roll;
-    private float pitch;
-    private float yaw;
+    private float thrustForce;
 
-    public float mass;
-    private Rigidbody rb;
+    // Aerodynamics
+    public float dragOverSpeedMod = 0.0005f;
+    private float drag;
+    private float lowSpeedAccelDamp;
+    private float lowSpeedAccelDampMod = 0.01f;
 
-    // Wing lift
-    private bool canGenLift;
-    public float AOAStallAngle;
 
     // Proplers
     private bool propState;
-    private bool propSwap;
     public GameObject[] propHolders;
     public GameObject[] proplers;
     public GameObject[] fakeProplers;
     private bool previousPropState;
 
-    public float maxRPM;
-    private float currentRPM = 0f;
+    public float propSpinSpeed = 13;
+    private float currentSpinSpeed = 0f;
     public float fakePropThreshold = 0.2f;
 
     // UI
     public TextMeshProUGUI throttleInd;
     public TextMeshProUGUI AirspeedInd;
-
-
-    public float responseModifier
-    {
-        get
-        {
-            rb.mass = mass;
-            return (rb.mass / 10f * responsiveness);
-        }
-    }
     public void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.mass = mass_Kg;
+        rb.automaticCenterOfMass = false;
     }
     public void Update()
     {
         HandleInputs();
+        RotateControlSurfaces();
+        UpdateUI();
     }
     public void HandleInputs()
     {
-        roll = Input.GetAxis("Roll");
-        pitch = Input.GetAxis("Pitch");
-        yaw = Input.GetAxis("Yaw");
+        // WASDQE input
+        roll = UnityEngine.Input.GetAxis("Roll");
+        pitch = UnityEngine.Input.GetAxis("Pitch");
+        yaw = UnityEngine.Input.GetAxis("Yaw");
 
-        if (Input.GetKey(KeyCode.LeftShift))
+        // Throttle input
+        if (UnityEngine.Input.GetKey(KeyCode.LeftShift))
         {
             throttle += throttleIncrement * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.LeftControl))
+        if (UnityEngine.Input.GetKey(KeyCode.LeftControl))
         {
             throttle -= throttleIncrement * Time.deltaTime;
         }
         throttle = Mathf.Clamp(throttle, 0f, 100f);
 
-        // UI
-        throttleInd.text = throttle.ToString("F0") + "%" ;
-        AirspeedInd.text = (rb.linearVelocity.magnitude * 3.6f).ToString("F0") + "KM/H";
+        // Flap input
+        if (UnityEngine.Input.GetKeyDown(KeyCode.F))
+        {
+            ChangeFlaps();
+        }
+    }
+    public void RotateControlSurfaces()
+    {
+        float aileronDelta = roll * controlSurfRotSpeed * Time.deltaTime;
+
+        currentAileronAngle = Mathf.Clamp(currentAileronAngle + aileronDelta, -aileronMaxRot, aileronMaxRot);
+
+        Vector3 changeAngles = aileronR.transform.localEulerAngles;
+        changeAngles.x = -currentAileronAngle;
+        aileronR.transform.localEulerAngles = changeAngles;
+        aileronL.transform.localEulerAngles = -changeAngles;
+    }
+    public void ChangeFlaps()
+    {
+
     }
     public void FixedUpdate()
     {
-        // Check AOA
-        Quaternion minQ = Quaternion.Euler(AOAStallAngle, 0, 0);
-        Quaternion maxQ = Quaternion.Euler(-AOAStallAngle, 0, 0);
-        Quaternion currentQ = transform.localRotation;
-        float minToCurrent = Quaternion.Angle(minQ, currentQ);
-        float currentToMax = Quaternion.Angle(currentQ, maxQ);
-        float minToMax = Quaternion.Angle(minQ, maxQ);
-        canGenLift = minToCurrent + currentToMax <= minToMax + 0.01f;
-        if (canGenLift)
+        CalculateForce();
+
+        ApplyForces();
+
+        RotatePropellors();
+    }
+
+    public void CalculateForce()
+    {
+        // HP to Newtons
+        float speed = rb.linearVelocity.magnitude;
+        float powerWatts = enginePower_Hp * 745.69f;
+
+        // Avoid divide-by-zero with a small clamp
+        speed = Mathf.Max(speed, 0.1f);
+
+        thrustForce = powerWatts / speed;
+
+        lowSpeedAccelDamp = 0.2f + rb.linearVelocity.magnitude * lowSpeedAccelDampMod;
+        lowSpeedAccelDamp = Mathf.Clamp01(lowSpeedAccelDamp);
+    }
+    public void ApplyForces()
+    {
+        // Float that converts Thrust to Thrust with Accel and Decel
+        if (throttle > currentThrust)
         {
-            rb.AddForce(transform.up * rb.linearVelocity.magnitude * liftMultiplier);
+            currentThrust += accelerationRate * Time.deltaTime;
+            if (currentThrust > throttle) currentThrust = throttle; 
+        }
+        else if (throttle < currentThrust)
+        {
+            currentThrust -= decelerationRate * Time.deltaTime;
+            if (currentThrust < throttle) currentThrust = throttle; 
         }
 
-        // Add input force
-        float targetThrust = maxThrust * throttle;
-        float rate = (targetThrust > currentThrust) ? accelerationRate * 1000f : decelerationRate * 1000f;
-        currentThrust = Mathf.MoveTowards(currentThrust, targetThrust, rate * Time.fixedDeltaTime);
+        // Apply forces
+        rb.AddForce(transform.up * rb.linearVelocity.magnitude * liftMultiplier);
+        rb.AddForce(transform.forward * thrustForce * currentThrust * lowSpeedAccelDamp);
+        rb.AddTorque(transform.up * yaw * yawResponsiveness * 10000f);
+        rb.AddTorque(transform.right * pitch * pitchResponsiveness * 10000f);
+        rb.AddTorque(-transform.forward * roll * rollResponsiveness * 10000f);
 
-        rb.AddForce(transform.forward * currentThrust);
-        rb.AddTorque(transform.up * yaw * responseModifier);
-        rb.AddTorque(transform.right * pitch * responseModifier);
-        rb.AddTorque(-transform.forward * roll * responseModifier);
-
+        // Simulate realistic drag by dynamicly increasing drag over speed
+        drag = 1.0f + rb.linearVelocity.magnitude * dragOverSpeedMod;
+        rb.linearDamping = drag;
+        rb.angularDamping = drag;
+    }
+    public void RotatePropellors()
+    {
         // Proplers 
         if (throttle >= 100f * fakePropThreshold)
         {
@@ -127,12 +179,19 @@ public class Flightcontroller : MonoBehaviour
             previousPropState = propState;
         }
 
-        currentRPM = Mathf.Lerp(currentRPM, throttle * maxRPM, Time.deltaTime * 10f);
-        float rotationPerFrame = (currentRPM / 60f) * 360f * Time.deltaTime;
+        currentSpinSpeed = Mathf.Lerp(currentSpinSpeed, throttle * propSpinSpeed, Time.deltaTime * 10f);
+        float rotationPerFrame = (currentSpinSpeed / 60f) * 360f * Time.deltaTime;
 
         foreach (GameObject obj in propHolders)
         {
             obj.transform.Rotate(Vector3.forward, rotationPerFrame);
         }
+    }
+
+    public void UpdateUI()
+    {
+        throttleInd.text = throttle.ToString("F0") + "%";
+
+        AirspeedInd.text = (rb.linearVelocity.magnitude * 3.6f).ToString("F0") + "KM/H";
     }
 }
